6b828b9c27c24fb42beedf57a5bf907e82fd17c4
axellbustos <axell.bustos@hotmail.com.ar>
2022-08-27 14:52:15 -0300

    CRUD 1

---
+++ src/controllers/mainController.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = { 
+	index: (req, res) => {
+		let productsVisited= products.filter(product=> product.category === 'visited')
+		let productsInsale= products.filter(product=> product.category === 'in-sale')
+		return res.render('index',{
+			productsVisited,
+			productsInsale,
+		})
+	},
+	search: (req, res) => {
+		// Do the magic
+	},
+};
+
+module.exports = controller;
---
+++ src/controllers/productsController.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const storeProducts= (products)=>{
+	fs.writeFileSync(productsFilePath,JSON.stringify(products,null,3),'utf-8')};
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		return res.render('products',{
+			products
+		})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const product=products.find(product=> product.id === +req.params.id)
+		return res.render('detail',{
+			product,
+			toThousand
+		})
+	},
+	// Create - Form to create
+	create: (req, res) => {
+		return res.render('product-create-form')
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const id= products[products.length - 1].id
+		const {name, price, discount}=req.body
+		const newProduct={
+			id:+id + 1,
+			...req.body,
+			name: name.trim(),
+			price:+price,
+			discount:+discount
+		}
+		return res.send('El producto se ha agregado con exito')
+		const productModify=[...products,newProduct]
+		storeProducts(productModify)
+        return res.redirect('/')
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		const productToEdit=products.find(product=> product.id === +req.params.id)
+		return res.render ('product-edit-form',{
+			productToEdit
+		})
+	},
+
+	// Update - Method to update
+	update: (req, res) => {
+		const {name, price,discount, description, category}= req.body
+        const {id}= req.params
+
+        const productModify= products.map(product => {
+            if(product.id === +id){
+                return{
+                    ...product,
+                    name:name.trim(),
+                    price: +price,
+                    discount: +discount,
+                    category,
+                    description
+                }
+            }
+            return product
+        })
+		return res.send('El producto fue editado con exito')
+        storeProducts(productModify)
+        return res.redirect('/')
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		const productDelete=products.filter(product=> product.id !== +req.params.id)
+		return res.send('El producto fue eliminado')
+		storeProducts(productDelete)
+		return res.redirect('/')
+	}
+};
+
+module.exports = controller
